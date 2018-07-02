9f1de7794987ff10c2d45fda01fffb2105a21da8
Jorge Espinosa <jorge.espinosacam@gmail.com>
2018-07-02 08:52:14 +0200

    Añadir header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PresentationViewerComponent } from './presentation-viewer/presentation-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentationViewerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/presentation-viewer/presentation-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation-viewer',
+  templateUrl: './presentation-viewer.component.html',
+  styleUrls: ['./presentation-viewer.component.css']
+})
+export class PresentationViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
