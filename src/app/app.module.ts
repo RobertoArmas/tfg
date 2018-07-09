f0de1f3336205ce7eb0e602eeea6698bc7aa40b4
Jorge Espinosa <jorge.espinosacam@gmail.com>
2018-07-09 08:26:03 +0200

    Añadir estructura del sidenav de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
-import { PresentationViewerComponent } from './presentation-viewer/presentation-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PresentationViewerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
