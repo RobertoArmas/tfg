4ec7917dfe4f23672a40b7e32e6da692bd0dcaf7
Jorge Espinosa <jespinosa@atnova.com>
2018-07-17 18:38:07 +0200

    Empezar botón de navegación a la siguiente lección

--- src/app/common/interactive/chunk-checkbox-list/chunk-checkbox-list.component.ts
+++ src/app/common/interactive/chunk-checkbox-list/chunk-checkbox-list.component.ts
@@ -10,14 +10,14 @@ export class ChunkCheckboxListComponent implements OnInit {
   @Input() attributes: CheckboxList;
   sampleItems: string[];
 
-  constructor() { 
+  constructor() {
     this.attributes = new CheckboxList();
     this.sampleItems = [
       // tslint:disable:max-line-length
       'Say yes more. Yes opens doors. No closes them. Yes pushes us. No keeps us safe at home. Imagine all the opportunities waiting for a yes.',
       'Let go of expectation. You’ve done your best to prepare. Now, let go. There’s no one way your life should unfold. Enjoy the journey.',
       'Welcome diversions. The most rewarding adventures often start with an unexpected detour. Perhaps that distraction will guide you onward.'
-    ]
+    ];
   }
 
   ngOnInit() {
