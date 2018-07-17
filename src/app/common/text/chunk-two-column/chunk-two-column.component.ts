881449a4e269f8d5b6a62ceacb5684257945d330
Jorge Espinosa <jespinosa@atnova.com>
2018-07-17 14:12:51 +0200

    Refactorizar chunks de heading + texto y añadir más datos de prueba

--- src/app/common/text/chunk-two-column/chunk-two-column.component.ts
+++ src/app/common/text/chunk-two-column/chunk-two-column.component.ts
@@ -15,8 +15,8 @@ export class ChunkTwoColumnComponent implements OnInit {
 
   ngOnInit() {
     // tslint:disable:max-line-length
-    if (this.attributes.dataCol1 === undefined) { this.attributes.dataCol1 = 'By all means, write your to-do list. Make careful arrangements. Choose your traveling companions. Pack up your talismans. And then scrap your well-thought out plan when your intuition whispers, "This way."'; }
-    if (this.attributes.dataCol2 === undefined) { this.attributes.dataCol2 = 'We open ourselves to discovery by following our deepest questions. If we take them seriously, they teach us everything we want to know. If we abandon them, we forfeit a powerful chance to become someone new.'; }
+    if (this.attributes.dataCol1 === undefined) { this.attributes.dataCol1 = ''; }
+    if (this.attributes.dataCol2 === undefined) { this.attributes.dataCol2 = ''; }
     if (this.attributes.paddingTop === undefined) { this.attributes.paddingTop = 30; }
     if (this.attributes.paddingBottom === undefined) { this.attributes.paddingBottom = 30; }
     if (this.attributes.backgroundColor === undefined) { this.attributes.backgroundColor = '#ffffff'; }
