406d9317de46f3f1c354702bf31af5a5ba5c6045
Jorge Espinosa <jespinosa@atnova.com>
2018-08-17 11:42:45 +0200

    Crear AuthGuard de paso (sin seguridad)

--- src/app/course-viewer/course-viewer-routing.module.ts
+++ src/app/course-viewer/course-viewer-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 import { LessonDetailComponent } from './course-viewer/lesson-detail/lesson-detail.component';
+import { AuthGuard } from '../core/auth-guard.service';
 
 const routes: Routes = [
   {
     path: '',
     component: CourseViewerComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: ':sectionId/lesson/:lessonId',
