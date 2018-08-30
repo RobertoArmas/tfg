f02eb23074ed035d6d490ae04dfd31f62c2798a1
Jorge Espinosa <jespinosa@atnova.com>
2018-08-30 17:37:25 +0200

    Implementar responsive

--- src/app/chunks/text/Basic.ts
+++ src/app/chunks/text/Basic.ts
@@ -2,11 +2,11 @@ import { ChunkData } from '../chunk.model';
 
 export class Basic implements ChunkData {
     data: string;
-    statementData: string;
-    paddingTop: number;
-    paddingBottom: number;
-    backgroundColor: string;
-    reviewed: boolean;
+    statementData?: string;
+    paddingTop?: number;
+    paddingBottom?: number;
+    backgroundColor?: string;
+    reviewed?: boolean;
 
     constructor(values: Object = {}) {
         Object.assign(this, values);
--- src/app/chunks/text/chunk-heading-text/chunk-heading-text.component.ts
+++ src/app/chunks/text/chunk-heading-text/chunk-heading-text.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { HeadingText } from '../HeadingText';
 import { IntersectionObserverService } from '../../../core/intersection-observer/intersection-observer.service';
 import { LessonData } from '../../../course-viewer/lesson.model';
+import { Basic } from '../Basic';
 
 @Component({
   selector: 'app-chunk-heading-text',
   templateUrl: './chunk-heading-text.component.html',
-  styleUrls: ['./chunk-heading-text.component.css']
+  styleUrls: ['./chunk-heading-text.component.scss']
 })
 export class ChunkHeadingTextComponent implements OnInit, AfterViewInit {
   @Input() attributes: HeadingText;
@@ -20,17 +21,16 @@ export class ChunkHeadingTextComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    // tslint:disable:max-line-length
-    if (this.attributes.headingData === undefined) { this.attributes.headingData = 'Combined title and paragraph'; }
-    if (this.attributes.textData === undefined) { this.attributes.textData = 'Proin consectetur at dui in semper. Nam molestie sit amet urna ut eleifend. In congue sapien erat, non congue dolor mattis sit amet. Suspendisse potenti. Mauris sed tempor lectus. Nullam gravida urna mattis erat sollicitudin, et lacinia ipsum porttitor. Nullam id eros ultricies, eleifend metus at, hendrerit massa.'; }
     if (this.attributes.paddingTop === undefined) { this.attributes.paddingTop = 30; }
     if (this.attributes.paddingBottom === undefined) { this.attributes.paddingBottom = 0; }
     if (this.attributes.backgroundColor === undefined) { this.attributes.backgroundColor = '#ffffff'; }
   }
 
-  getHeadingAttributes() {
+  getHeadingAttributes(): Basic {
     return {
-      data: this.attributes.headingData
+      data: this.attributes.headingData,
+      paddingTop: 8,
+      paddingBottom: 8
     };
   }
 
