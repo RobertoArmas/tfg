da06b4d8de6316b4858b5e7ba20b35f4aba53a06
Jorge Espinosa <jespinosa@atnova.com>
2018-08-30 17:37:25 +0200

    Implementar responsive

--- src/app/chunks/chunk.model.ts
+++ src/app/chunks/chunk.model.ts
@@ -1,11 +1,11 @@
 import { LessonData } from '../course-viewer/lesson.model';
 
 export interface ChunkData {
-    paddingTop: number;
-    paddingBottom: number;
-    backgroundColor: string;
-    statementData: string;
-    reviewed: boolean;
+    paddingTop?: number;
+    paddingBottom?: number;
+    backgroundColor?: string;
+    statementData?: string;
+    reviewed?: boolean;
 }
 
 export class Chunk {
--- src/app/chunks/text/chunk-subheading/chunk-subheading.component.ts
+++ src/app/chunks/text/chunk-subheading/chunk-subheading.component.ts
@@ -6,7 +6,7 @@ import { LessonData } from '../../../course-viewer/lesson.model';
 @Component({
   selector: 'app-chunk-subheading',
   templateUrl: './chunk-subheading.component.html',
-  styleUrls: ['./chunk-subheading.component.css']
+  styleUrls: ['./chunk-subheading.component.scss']
 })
 export class ChunkSubheadingComponent implements OnInit, AfterViewInit {
   @Input() attributes: Basic;
@@ -20,7 +20,6 @@ export class ChunkSubheadingComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    if (this.attributes.data === undefined) { this.attributes.data = 'Subheading'; }
     if (this.attributes.paddingTop === undefined) { this.attributes.paddingTop = 30; }
     if (this.attributes.paddingBottom === undefined) { this.attributes.paddingBottom = 0; }
     if (this.attributes.backgroundColor === undefined) { this.attributes.backgroundColor = '#ffffff'; }
