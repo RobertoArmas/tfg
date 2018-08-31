109709c80b318971d9699f3cf32733dac0a5fa07
Jorge Espinosa <jespinosa@atnova.com>
2018-08-31 14:00:13 +0200

    Empezar refactor de la imagen centrada

--- src/app/chunks/interactive/chunk-checkbox-list/chunk-checkbox-list.component.ts
+++ src/app/chunks/interactive/chunk-checkbox-list/chunk-checkbox-list.component.ts
@@ -6,7 +6,7 @@ import { LessonData } from '../../../course-viewer/lesson.model';
 @Component({
   selector: 'app-chunk-checkbox-list',
   templateUrl: './chunk-checkbox-list.component.html',
-  styleUrls: ['./chunk-checkbox-list.component.css']
+  styleUrls: ['./chunk-checkbox-list.component.scss']
 })
 export class ChunkCheckboxListComponent implements OnInit, AfterViewInit {
   @Input() attributes: CheckboxList;
@@ -18,16 +18,9 @@ export class ChunkCheckboxListComponent implements OnInit, AfterViewInit {
     private intersectionObserverService: IntersectionObserverService
   ) {
     this.attributes = new CheckboxList();
-    this.sampleItems = [
-      // tslint:disable:max-line-length
-      'Say yes more. Yes opens doors. No closes them. Yes pushes us. No keeps us safe at home. Imagine all the opportunities waiting for a yes.',
-      'Let go of expectation. You’ve done your best to prepare. Now, let go. There’s no one way your life should unfold. Enjoy the journey.',
-      'Welcome diversions. The most rewarding adventures often start with an unexpected detour. Perhaps that distraction will guide you onward.'
-    ];
   }
 
   ngOnInit() {
-    if (this.attributes.items === undefined) { this.attributes.items = this.sampleItems; }
     if (this.attributes.paddingTop === undefined) { this.attributes.paddingTop = 30; }
     if (this.attributes.paddingBottom === undefined) { this.attributes.paddingBottom = 0; }
     if (this.attributes.backgroundColor === undefined) { this.attributes.backgroundColor = '#ffffff'; }
