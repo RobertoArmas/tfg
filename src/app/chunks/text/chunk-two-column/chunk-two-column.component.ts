65c56e54f7fba0ace1936a02198551da7231534d
Jorge Espinosa <jespinosa@atnova.com>
2018-08-31 13:11:32 +0200

    Refactorizar chunk two column

--- src/app/chunks/text/chunk-two-column/chunk-two-column.component.ts
+++ src/app/chunks/text/chunk-two-column/chunk-two-column.component.ts
@@ -7,7 +7,7 @@ import { LessonData } from '../../../course-viewer/lesson.model';
 @Component({
   selector: 'app-chunk-two-column',
   templateUrl: './chunk-two-column.component.html',
-  styleUrls: ['./chunk-two-column.component.css']
+  styleUrls: ['./chunk-two-column.component.scss']
 })
 export class ChunkTwoColumnComponent implements OnInit, AfterViewInit {
   @Input() attributes: TwoCol;
@@ -22,8 +22,6 @@ export class ChunkTwoColumnComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     // tslint:disable:max-line-length
-    if (this.attributes.dataCol1 === undefined) { this.attributes.dataCol1 = ''; }
-    if (this.attributes.dataCol2 === undefined) { this.attributes.dataCol2 = ''; }
     if (this.attributes.paddingTop === undefined) { this.attributes.paddingTop = 30; }
     if (this.attributes.paddingBottom === undefined) { this.attributes.paddingBottom = 30; }
     if (this.attributes.backgroundColor === undefined) { this.attributes.backgroundColor = '#ffffff'; }
