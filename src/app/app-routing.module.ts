bfa8436ae96f88a4c01581f6ada9ff64c4c1075e
Jorge Espinosa <jorge.espinosacam@gmail.com>
2018-09-26 10:55:56 +0200

    Implementar autenticación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
-import { AuthGuard } from './core/auth-guard.service';
 
 /**
  * Rutas de la aplicación a nivel global
