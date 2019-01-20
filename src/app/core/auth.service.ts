d26491ffd0b37b517bdc2eff440738f2249a7a50
Jorge Espinosa <jorge.espinosacam@gmail.com>
2019-01-20 10:43:26 +0100

    Añadir imagen de perfil del usuario dentro del curso

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -11,20 +11,22 @@ export class AuthService {
 
   redirectUrl: string;
   private _userId: string; // Usar la userid resuelta para evitar ForkJoins
+  private profilePicture$: string;
 
   constructor(private fbsAuth: AngularFireAuth, private router: Router) {
     this.uid.subscribe(
       uid => {
         this._userId = uid;
       }
-    )
+    );
 
     // Cuando el usuario logea correctamente redirecciona a la url que quería activar
     this.fbsAuth.auth.onAuthStateChanged(
       user => {
-        if(user) {
+        if (user) {
           this.router.navigate([this.redirectUrl]);
           this._userId = user.uid;
+          this.profilePicture$ = user.photoURL;
         }
       }
     );
@@ -34,25 +36,29 @@ export class AuthService {
     return this._userId;
   }
 
+  get profilePicture() {
+    return this.profilePicture$;
+  }
+
   get uid(): Observable<string> {
     return this.fbsAuth.user.pipe(
       take(1),
-      map(user => {return user.uid})
+      map(user => user.uid)
     );
   }
 
   get name(): Observable<string> {
     return this.fbsAuth.user.pipe(
       take(1),
-      map(user => {return user.displayName})
-    )
+      map(user => user.displayName)
+    );
   }
 
   checkLogin(): Observable<boolean> {
     return this.fbsAuth.user.pipe(
       map(
         user => {
-          if(!user) {
+          if (!user) {
             this.fbsAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
             return false;
           } else {
