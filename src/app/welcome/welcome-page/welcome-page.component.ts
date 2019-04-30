e6021da2e823783970f902c5bf7e6f1da31a87f3
Jorge Espinosa <jorge.espinosacam@gmail.com>
2019-04-30 19:22:59 +0200

    Modificar página de bienvenida a primera versión

--- src/app/welcome/welcome-page/welcome-page.component.ts
+++ src/app/welcome/welcome-page/welcome-page.component.ts
@@ -7,23 +7,43 @@ import { XapiService } from '../../core/xapi/xapi.service';
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
+  styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
 
   course: CourseData;
 
+  courses: CourseData[];
+
   constructor(
     private xapi: XapiService,
     private courseDataService: CourseDataService
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.getCourseInformation();
+    this.getCoursesData();
   }
-
+  getCoursesData() {
+    this.courseDataService
+      .getCoursesData()
+      .subscribe(
+        (courses) => {
+          this.courses = courses;
+          this.courses.forEach(course => {
+            const lessons = [];
+            this.courseDataService.getCourseNumberOfLessons(course.id)
+              .subscribe(
+                numberOfLessons => {
+                  lessons.push(numberOfLessons);
+                  course.NumberOfLessons = lessons.length;
+                }
+              );
+          });
+        }
+      );
+  }
   getCourseInformation() {
-
     this.courseDataService
       .getCourseInformation()
       .subscribe(
@@ -39,4 +59,16 @@ export class WelcomePageComponent implements OnInit {
     this.xapi.started(this.course);
   }
 
+
+  getCourseNumberOfLessons(courseId) {
+    const NumberOfLessonsSubscription = this.courseDataService.getCourseNumberOfLessons(courseId)
+      .subscribe(
+        numberOfLessons => {
+          console.log(numberOfLessons.length);
+          return numberOfLessons.length;
+        }
+      );
+
+    // NumberOfLessonsSubscription.unsubscribe();
+  }
 }
